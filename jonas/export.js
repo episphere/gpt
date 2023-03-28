4701f7d2fb727e374a1c29f18a3b9014cc1a40c6
Jonas Almeida <jalmeida@mathbiol.org>
2023-03-28 15:41:38 -0400

    check for key

--- jonas/export.js
+++ jonas/export.js
@@ -1,1 +1,28 @@
 // exploring GPT APIs
+
+console.log('jonas/export.js imported')
+
+const hello=`hello GPT at ${Date()}`;
+
+var key = null //'null'
+
+function check4key(){
+    const msgTrue = 'API key found, provided to SDK'
+    const msgFalse = 'No GPT API Key found, please generate one at https://platform.openai.com/account/api-keys'
+    if((localStorage.GPT_API_key)&&(localStorage.GPT_API_key!='null')&&(localStorage.GPT_API_key.length>0)){
+        console.log(msgTrue)
+    }else{
+        console.log(msgFalse)
+        localStorage.GPT_API_key=prompt(msgFalse+' and provide it here: ')
+    }
+    key=localStorage.GPT_API_key
+    // delete localStorage.GPT_API_key // if this machine cannot be trusted with a persistent API key
+}
+
+check4key()
+
+export{
+    hello,
+    key,
+    check4key
+}
