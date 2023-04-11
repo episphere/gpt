85bbe3994fe77eea93550426e26943e3a0c8db97
Jonas Almeida <jalmeida@mathbiol.org>
2023-04-11 13:55:33 -0400

    accept key via hash

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Wrangling Interactive UI stuff
 
-console.log(`${location.href}index.js loaded \n${Date()}`);
+console.log(`${location.origin+location.pathname}index.js loaded \n${Date()}`);
 
 (async function(){
+    // check for key as a hash first
+    let parms={}
+    location.hash.slice(1).split('&').forEach(av=>{
+        av=av.split('=')
+        parms[av[0]]=av[1]
+    })
+    //console.log(parms)
+    if(parms.key){
+        localStorage.GPT_API_key=parms.key
+        location.hash=location.hash.replace(`key=${parms.key}`,'') // extract key
+        location.hash=location.hash.replace('#&','').replace('&&','&') //clean-up
+    }
     gpt = await import('./export.js')
     gpt.chatUI()
 })()
