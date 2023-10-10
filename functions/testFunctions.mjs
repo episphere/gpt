4d583bae0604a73703837239cb6a4057472da875
Jonas Almeida <jalmeida@mathbiol.org>
2023-10-10 12:11:49 -0400

    removing hello and bye

--- functions/testFunctions.mjs
+++ functions/testFunctions.mjs
@@ -4,43 +4,6 @@
 
 const loaded=Date()
 
-function say_hello(date){
-    console.log(`hello functions at`,date)
-    console.log(`signed in to functions at ${Date()}`)
-}
-const hello = {
-    "name": "say_hello",
-    "description": "say hello to functions",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "date": {
-                "type": "string",
-                "description": "date string"
-            },
-        }
-    }
-}
-
-function say_goodbye(date){
-    console.log(`goodbye functions at`,date)
-    console.log(`signed out of functions at ${Date()}`)
-}
-const bye = {
-    "name": "say_goodbye",
-    "description": "say goodbye to functions",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "date": {
-                "type": "string",
-                "description": "date string"
-            },
-        }
-    }
-}
-
-
 async function weather_nci(when){
     let x = await (await fetch('https://api.weather.gov/gridpoints/LWX/90,80/forecast')).json()
     let y = x.properties.periods.filter(xi=>xi.name==when.when)[0]
@@ -145,40 +108,6 @@ const rankSumTestDescription = {   // at NCI Shady Grove
         }
     }
 };
-
-/*
-async fetchUCSC = async function(pams){
-    parms = parms|{
-        genome:'hg38',
-        chr:1,
-        start:100000,
-        end:100100
-    }
-    genome=hg38&chrom=1&start=100000&end=100100
-}
-
-function hello(){
-    console.log(`hello functions at ${loaded}`)
-    return {
-        "name": "get_current_weather",
-        "description": "Get the current weather in a given location",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "location": {
-                    "type": "string",
-                    "description": "The city and state, e.g. San Francisco, CA"
-                },
-                "unit": {
-                    "type": "string",
-                    "enum": ["celsius", "fahrenheit"]
-              }
-            },
-            "required": ["location"]
-        }
-    }
-}
-*/
 
 export{
     hello,
