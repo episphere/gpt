f43f8d5e45b25a55b5e63f68e88b205a58a28bb0
Jonas Almeida <jalmeida@mathbiol.org>
2024-02-06 11:01:41 -0500

    Create funFetchUCSC.js

---
+++ functions/funFetchUCSC.js
@@ -0,0 +1,24 @@
+async function funFetchUCSC(parms){
+    parms = parms||{
+        genome:'hg38',
+        chrom:'chr1',
+        start:100000,
+        end:100100
+    }
+    parms.genome=parms.genome||'hg38' // default assembly
+    console.log('parms:',parms)
+    //genome=hg38&chrom=1&start=100000&end=100100
+    let url = 'https://api.genome.ucsc.edu/getData/sequence'
+    Object.keys(parms).forEach((k,i)=>{
+        if(i==0){
+            url+=`?${k}=${parms[k]}`
+        }else{
+            url+=`;${k}=${parms[k]}`
+        }
+    })
+    console.log(url)
+    let res = await (await fetch(url)).json()
+    return `The genomic sequence in that position is ${res.dna.toUpperCase()}`
+    //return JSON.stringify(res)
+    //debugger
+}
